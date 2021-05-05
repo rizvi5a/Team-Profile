e79de12d27cdded81ed524234a9fec160df716f0
syed Rizvi <rizvi5a@yahoo.com>
2021-05-05 00:22:49 -0500

    one test is checked on employee name

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee= require('../lib/Employee');
+
+test('Check an employee is created', () => {
+
+let employee =new Employee ("name", "id", "name@email");
+
+  expect(employee.getName()).toMatch("name");
+ // expect(employee.getId()).toMatch("Id");
+});
